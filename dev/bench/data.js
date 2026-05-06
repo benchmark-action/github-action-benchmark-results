2b26ee490948996b913d5d39e6fd5342ba9259d8
github-action-benchmark <github@users.noreply.github.com>
2026-05-06 09:41:09 +0000

    add Python Benchmark with pytest-benchmark (pytest) benchmark result for 962496030412810aa8addc05a8c4853a082039de

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1778060457457,
+  "lastUpdate": 1778060469135,
   "entries": {
     "Rust Benchmark": [
       {
@@ -44343,6 +44343,44 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.00019203726257766742",
             "extra": "mean: 1.7793886907019063 msec\nrounds: 527"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "962496030412810aa8addc05a8c4853a082039de",
+          "message": "chore(deps): bump uuid and @actions/core (#350)\n\nRemoves [uuid](https://github.com/uuidjs/uuid). It's no longer used\nafter updating ancestor dependency\n[@actions/core](https://github.com/actions/toolkit/tree/HEAD/packages/core).\nThese dependencies need to be updated together.\n\nRemoves `uuid`\n\nUpdates `@actions/core` from 1.10.0 to 1.11.1\n<details>\n<summary>Changelog</summary>\n<p><em>Sourced from <a\nhref=\"https://github.com/actions/toolkit/blob/main/packages/core/RELEASES.md\">@​actions/core's\nchangelog</a>.</em></p>\n<blockquote>\n<h2>1.11.1</h2>\n<ul>\n<li>Fix uses of <code>crypto.randomUUID</code> on Node 18 and earlier <a\nhref=\"https://redirect.github.com/actions/toolkit/pull/1842\">#1842</a></li>\n</ul>\n<h3>1.11.0</h3>\n<ul>\n<li>Add platform info utilities <a\nhref=\"https://redirect.github.com/actions/toolkit/pull/1551\">#1551</a></li>\n<li>Remove dependency on <code>uuid</code> package <a\nhref=\"https://redirect.github.com/actions/toolkit/pull/1824\">#1824</a></li>\n</ul>\n<h3>1.10.1</h3>\n<ul>\n<li>Fix error message reference in oidc utils <a\nhref=\"https://redirect.github.com/actions/toolkit/pull/1511\">#1511</a></li>\n</ul>\n</blockquote>\n</details>\n<details>\n<summary>Commits</summary>\n<ul>\n<li>See full diff in <a\nhref=\"https://github.com/actions/toolkit/commits/HEAD/packages/core\">compare\nview</a></li>\n</ul>\n</details>\n<br />\n\nSigned-off-by: dependabot[bot] <support@github.com>\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2026-05-06T11:39:49+02:00",
+          "tree_id": "127834cc4435587ac9ed9e12c81de36b4a1f8238",
+          "url": "https://github.com/benchmark-action/github-action-benchmark/commit/962496030412810aa8addc05a8c4853a082039de"
+        },
+        "date": 1778060444113,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "bench.py::test_fib_10",
+            "value": 80268.63198452616,
+            "unit": "iter/sec",
+            "range": "stddev: 9.109070803959315e-7",
+            "extra": "mean: 12.458166724365947 usec\nrounds: 60651"
+          },
+          {
+            "name": "bench.py::test_fib_20",
+            "value": 669.2533458993881,
+            "unit": "iter/sec",
+            "range": "stddev: 0.000039324234548205244",
+            "extra": "mean: 1.4942024662665407 msec\nrounds: 667"
           }
         ]
       }
